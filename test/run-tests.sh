824f802abaad1f24e44404a562d8b32e9f063b68
Julia Farias <juliaalfarias@gmail.com>
2022-09-22 16:19:05 -0300

    Install setuptools

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -7,5 +7,7 @@ python3 -m pip install -r requirements.txt
 python3 -m pip install pytest
 python3 -m pip install pytest-cov
 python3 -m pip install cryptography==38.0.1
+python3 -m pip install setuptools==65.3.0
+python3 -m pip install setuptools-rust==1.1.2
 
 pytest
